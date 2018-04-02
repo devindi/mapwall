d773993369359eeb4fe3d0a1a042484b0bceb0d0
Andrew <andrew@devindi.com>
2018-04-03 00:11:34 +0300

    Implemented tile concatenation

--- app/src/main/java/com/devindi/wallpaper/preview/PreviewController.kt
+++ app/src/main/java/com/devindi/wallpaper/preview/PreviewController.kt
@@ -10,8 +10,7 @@ import com.devindi.wallpaper.R
 import org.osmdroid.tileprovider.cachemanager.CacheManager
 import org.osmdroid.tileprovider.modules.SqlTileWriter
 import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
-import org.osmdroid.tileprovider.tilesource.ThunderforestTileSource
-import org.osmdroid.util.MapTileIndex
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.MapView
 
 class PreviewController : Controller() {
@@ -22,14 +21,17 @@ class PreviewController : Controller() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
         val view = inflater.inflate(R.layout.preview_screen, container, false)
         map = view.findViewById(R.id.map_view)
-        val tileSource = ThunderforestTileSource(container.context, ThunderforestTileSource.SPINAL_MAP)
-//        val tileSource = TileSourceFactory.DEFAULT_TILE_SOURCE
+        map.isVerticalMapRepetitionEnabled = false
+        map.isHorizontalMapRepetitionEnabled = false
+        map.setMultiTouchControls(true)
+        map.setBuiltInZoomControls(false)
+        val tileSource = TileSourceFactory.DEFAULT_TILE_SOURCE
         val factory = createFactory(tileSource)
         map.setTileSource(tileSource)
         val btn = view.findViewById<View>(R.id.button)
         btn.setOnClickListener {
             Thread(Runnable {
-                consumer.setWallpaper(factory.createWallpaper(tileSource, MapTileIndex.getTileIndex(8, 149, 74)))
+                consumer.setWallpaper(factory.createWallpaper(map.boundingBox, map.zoomLevel))
             }).start()
         }
         return view
@@ -48,6 +50,6 @@ class PreviewController : Controller() {
 
     private fun createFactory(tileSource: OnlineTileSourceBase):WallpaperFactory {
         val cache = SqlTileWriter()
-        return WallpaperFactory(CacheManager(tileSource, cache, 0, 19), cache)
+        return WallpaperFactory(CacheManager(tileSource, cache, tileSource.minimumZoomLevel, tileSource.maximumZoomLevel), tileSource, cache)
     }
 }
--- app/src/main/java/com/devindi/wallpaper/preview/WallpaperFactory.kt
+++ app/src/main/java/com/devindi/wallpaper/preview/WallpaperFactory.kt
@@ -1,19 +1,38 @@
 package com.devindi.wallpaper.preview
 
 import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
 import org.osmdroid.tileprovider.cachemanager.CacheManager
 import org.osmdroid.tileprovider.modules.IFilesystemCache
 import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase
+import org.osmdroid.util.BoundingBox
+import org.osmdroid.util.MapTileIndex
 
-class WallpaperFactory(private val cacheManager: CacheManager, private val cache: IFilesystemCache) {
+class WallpaperFactory(private val cacheManager: CacheManager, private var tileSource: OnlineTileSourceBase, private val cache: IFilesystemCache) {
 
-    fun createWallpaper(tileSource: OnlineTileSourceBase, tileIndex: Long): Bitmap {
-        val loaded = cacheManager.loadTile(tileSource, tileIndex)
-        if (loaded) {
-            val drawable = cache.loadTile(tileSource, tileIndex) as BitmapDrawable
-            return drawable.bitmap
+    fun createWallpaper(rect: BoundingBox, zoom: Int): Bitmap {
+        var tilesCoverage = CacheManager.getTilesCoverage(rect, zoom)
+        tilesCoverage.forEach {
+            if (cacheManager.downloadingAction.tileAction(it)) {
+                throw IllegalStateException("Failed to load tile")
+            }
         }
-        else throw IllegalStateException("Failed to load tile")
+        tilesCoverage = tilesCoverage.sortedWith(compareBy({ MapTileIndex.getX(it) }, { MapTileIndex.getY(it) }))
+        val startX = MapTileIndex.getX(tilesCoverage[0])
+        val tilesY = tilesCoverage
+                .filter { MapTileIndex.getX(it) == startX }
+                .size
+        val tilesX = tilesCoverage.size / tilesY
+        val tileSize = tileSource.tileSizePixels
+        val target = Bitmap.createBitmap(tilesX * tileSize, tilesY * tileSize, Bitmap.Config.RGB_565)
+        val canvas = Canvas(target)
+        for (i in 0 until tilesCoverage.size) {
+            val pxLeft = (i / tilesY) * tileSize.toFloat()
+            val pxTop = (i % tilesY) * tileSize.toFloat()
+            val drawable = cache.loadTile(tileSource, tilesCoverage[i]) as BitmapDrawable
+            canvas.drawBitmap(drawable.bitmap, pxLeft, pxTop, null)
+        }
+        return target
     }
 }
