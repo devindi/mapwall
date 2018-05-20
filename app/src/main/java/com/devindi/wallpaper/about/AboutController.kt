d4d4cb6de006f9b1a3e6b6aa4f68be452df8d425
Andrew <andrew@devindi.com>
2018-05-21 00:18:05 +0300

    Improved about screen and icon

--- app/src/main/java/com/devindi/wallpaper/about/AboutController.kt
+++ app/src/main/java/com/devindi/wallpaper/about/AboutController.kt
@@ -5,11 +5,14 @@ import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.bluelinelabs.conductor.archlifecycle.LifecycleController
+import com.devindi.wallpaper.BuildConfig
 import com.devindi.wallpaper.R
 import com.devindi.wallpaper.misc.ReportManager
 import com.devindi.wallpaper.misc.inject
 import com.devindi.wallpaper.model.analytics.ScreenEvent
+import java.util.*
 
 class AboutController: LifecycleController() {
 
@@ -24,6 +27,11 @@ class AboutController: LifecycleController() {
             it.supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
         toolbar.setNavigationOnClickListener { router.popCurrentController() }
+
+        val versionString = String.format(Locale.US,
+                container.context.getText(R.string.about_version).toString(),
+                BuildConfig.VERSION_NAME)
+        view.findViewById<TextView>(R.id.about_version).text = versionString
 
         return view
     }
