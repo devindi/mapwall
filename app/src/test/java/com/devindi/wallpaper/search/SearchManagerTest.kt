0b9aa0a7c0ee97390fa5ce219d0d74cfd9273bae
Eugene Dudnik <esdudnik@gmail.com>
2018-10-09 20:09:18 +0300

    fixes with mocked iterator

--- app/src/test/java/com/devindi/wallpaper/search/SearchManagerTest.kt
+++ app/src/test/java/com/devindi/wallpaper/search/SearchManagerTest.kt
@@ -3,11 +3,15 @@ package com.devindi.wallpaper.search
 import com.devindi.wallpaper.model.places.GoogleApiErrorHandler
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.common.api.PendingResult
+import com.google.android.gms.common.api.ResultCallback
 import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.places.AutocompleteFilter
+import com.google.android.gms.location.places.AutocompletePrediction
 import com.google.android.gms.location.places.AutocompletePredictionBuffer
 import com.google.android.gms.location.places.GeoDataApi
 import com.nhaarman.mockito_kotlin.*
+import org.junit.Assert.assertArrayEquals
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
 
@@ -21,11 +25,12 @@ class SearchManagerTest {
     private val REQUEST_SUGGESTS_STRING = "REQUEST_SUGGESTS_STRING"
     private val REQUEST_PLACE_STRING = "REQUEST_PLACE_STRING"
     private val statusCanceled = Status.RESULT_CANCELED
+    private val statusSuccess = Status.RESULT_SUCCESS
 
     private val mockGeoDataApi: GeoDataApi = mock()
     private val mockApiClientApi: GoogleApiClient = mock()
     private val mockGoogleApiErrorHandler: GoogleApiErrorHandler = mock()
-    private val searchManagerTest = SearchManager(mockGeoDataApi, mockApiClientApi, mockGoogleApiErrorHandler)
+    private val searchManager = SearchManager(mockGeoDataApi, mockApiClientApi, mockGoogleApiErrorHandler)
 
     private val filter = AutocompleteFilter.Builder()
             .setTypeFilter(AutocompleteFilter.TYPE_FILTER_REGIONS)
@@ -33,7 +38,7 @@ class SearchManagerTest {
 
     private val mockAutocompletePredictionBuffer: AutocompletePredictionBuffer = mock()
     private val mockPendingResult: PendingResult<AutocompletePredictionBuffer> = mock()
-    private val resultCallBackCaptor = argumentCaptor<(AutocompletePredictionBuffer) -> Unit>()
+    private val resultCallBackCaptor = argumentCaptor<ResultCallback<AutocompletePredictionBuffer>>()
 
     @Before
     fun setup() {
@@ -46,10 +51,37 @@ class SearchManagerTest {
                 .thenReturn(mockPendingResult)
         whenever(mockAutocompletePredictionBuffer.status).thenReturn(statusCanceled)
 
-        searchManagerTest.requestSuggests(REQUEST_SUGGESTS_STRING)
+        searchManager.requestSuggests(REQUEST_SUGGESTS_STRING)
         verify(mockPendingResult).setResultCallback(resultCallBackCaptor.capture())
-        resultCallBackCaptor.firstValue.invoke(mockAutocompletePredictionBuffer)
+        resultCallBackCaptor.firstValue.onResult(mockAutocompletePredictionBuffer)
 
         verify(mockGoogleApiErrorHandler, times(1)).onErrorStatus(statusCanceled)
+    }
+
+    @Test
+    fun testRequestSuggestWhenSuccess() {
+        val predictions : MutableList<AutocompletePrediction> = mutableListOf(
+
+        )
+
+        whenever(mockGeoDataApi.getAutocompletePredictions(mockApiClientApi, REQUEST_SUGGESTS_STRING, null, filter))
+                .thenReturn(mockPendingResult)
+        whenever(mockAutocompletePredictionBuffer.status).thenReturn(statusSuccess)
+        whenever(mockAutocompletePredictionBuffer.iterator()).thenReturn(predictions.iterator())
+
+        searchManager.requestSuggests(REQUEST_SUGGESTS_STRING)
+        verify(mockPendingResult).setResultCallback(resultCallBackCaptor.capture())
+        resultCallBackCaptor.firstValue.onResult(mockAutocompletePredictionBuffer)
+
+        val expectedSuggestsList = predictions
+                .map { autocompletePrediction ->
+                    PlaceSuggest(
+                            REQUEST_SUGGESTS_STRING,
+                            autocompletePrediction.getPrimaryText(null).toString(),
+                            autocompletePrediction.getSecondaryText(null).toString(),
+                            autocompletePrediction.placeId)
+                }
+
+        assertArrayEquals(searchManager.suggests.value?.toTypedArray(), expectedSuggestsList.toTypedArray())
     }
 }
