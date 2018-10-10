9b0df37e869226720e799db7659981e3bbaa14ec
Andrew Mihin <andrew@devindi.com>
2018-10-10 22:10:34 +0300

    Add new image generator

---
+++ app/src/main/java/com/devindi/wallpaper/model/map/MapImageGenerator.kt
@@ -0,0 +1,13 @@
+package com.devindi.wallpaper.model.map
+
+import android.graphics.Bitmap
+
+/**
+ * Generates bitmap by params (source, center coordinates, zoom, size)
+ */
+class MapImageGenerator() {
+
+    fun generate(): Bitmap {
+        throw NotImplementedError()
+    }
+}
