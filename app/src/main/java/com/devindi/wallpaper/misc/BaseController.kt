82be6eafa79a15514933ac9b9efd154215c5387f
Andrew Mihin <andrew@devindi.com>
2019-01-23 21:56:57 +0300

    Improve view model store usage

--- app/src/main/java/com/devindi/wallpaper/misc/BaseController.kt
+++ app/src/main/java/com/devindi/wallpaper/misc/BaseController.kt
@@ -3,14 +3,21 @@ package com.devindi.wallpaper.misc
 import android.arch.lifecycle.ViewModelStore
 import android.arch.lifecycle.ViewModelStoreOwner
 import android.os.Bundle
-import android.support.v4.app.FragmentActivity
 import com.bluelinelabs.conductor.archlifecycle.LifecycleController
 
-abstract class BaseController(args: Bundle? = null) :
-    LifecycleController(args),
-    ViewModelStoreOwner {
+/**
+ * Inspired by https://github.com/miquelbeltran/conductor-viewmodel
+ */
+abstract class BaseController(args: Bundle? = null) : LifecycleController(args), ViewModelStoreOwner {
+
+    private val viewModelStore = ViewModelStore()
 
     override fun getViewModelStore(): ViewModelStore {
-        return (activity as FragmentActivity).viewModelStore
+        return viewModelStore
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewModelStore.clear()
     }
 }
